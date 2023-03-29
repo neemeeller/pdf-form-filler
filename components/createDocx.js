458761690f187ea53cfeb61ca735bbfd0dc34eb4
neemeeller <neemeeller@uptime.ee>
2023-03-29 17:02:01 +0300

    [docx] fill template form

---
+++ components/createDocx.js
@@ -0,0 +1,73 @@
+// const { Document, Packer, Paragraph } = require('docx');
+const createReport = require('docx-templates').default;
+const fs = require('fs');
+
+const createDocx = async (inputPath, outputPath) => {
+  if (!inputPath || !outputPath) {
+    throw new Error('No path specified');
+  }
+
+  try {
+    const template = fs.readFileSync(inputPath);
+
+    const buffer = await createReport({
+      template,
+      cmdDelimiter: ['[', ']'],
+      processLineBreaks: true,
+      failFast: false,
+      additionalJsContext: {
+        // all of these will be available to JS snippets in your template commands (see below)
+        _1_1: '',
+        _1_2: '',
+        _1_3: '',
+        _1_4: '',
+        _1_5: '',
+        _1_OTHER: '',
+        _2: '',
+        _2_ACC_NO: '',
+        _3: '',
+        _4: '',
+        _5: '',
+        _6: '',
+        _7: '',
+        _7_ACC_NO: '',
+        _page: '1',
+        _pages: '2023',
+      },
+      data: {
+        // Year: '2023',
+        // Subtitle: 'Meie elu lood',
+        _1_1: '',
+        _1_2: 'X',
+        _2: 'This is the from field',
+        // '_10.2': 'Second',
+      },
+
+    });
+
+    fs.writeFileSync(outputPath, buffer);
+  } catch (e) {
+    throw new Error(`Error occurred! `, e);
+  }
+
+  /* try {
+    const doc = new Document({
+      sections: [
+        {
+          children: [new Paragraph('Hello World')],
+        },
+      ],
+    });
+
+    Packer.toBuffer(doc).then(buffer => {
+      console.log(buffer);
+      fs.writeFileSync(outputPath, buffer);
+    });
+  } catch (e) {
+    throw new Error(`Error occurred! `, e);
+  } */
+};
+
+module.exports = {
+  createDocx,
+};
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // const { createPdf } = require('./components/createPdf');
-const { rtfReader } = require('./components/rtfReader');
+// const { rtfReader } = require('./components/rtfReader');
+const { createDocx } = require('./components/createDocx');
 
 // createPdf('./assets/input/f1099nec.pdf', './assets/output/output.pdf');
-rtfReader('./assets/input/SF153-88.rtf', './assets/output/SF153-88_out.rtf');
+// rtfReader('./assets/input/SF153-88.rtf', './assets/output/SF153-88_out.rtf');
+createDocx('./assets/input/input.docx', './assets/output/create_doc.docx');
